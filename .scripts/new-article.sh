cfb3382934c64df995d30ea6692c028d5069957f
alistaircol <aggenkeech@gmail.com>
2021-05-17 17:55:19 +0100

    updated command to get date https://unix.stackexchange.com/q/164826/265713

--- .scripts/new-article.sh
+++ .scripts/new-article.sh
@@ -4,6 +4,9 @@ read -rp 'Article Slug: ' slug
 read -rp 'Article Description: ' description
 read -rp 'Tags (csv with single quote): ' tags
 
+# realpath: command not found
+# ughh.
+# brew install coreutils
 script_directory="${BASH_SOURCE[0]:-$0}"
 realpath="$(realpath -s "$script_directory")"
 filename="$(dirname "$(dirname "$realpath")")/content/articles/$slug.md"
@@ -15,17 +18,8 @@ cat <<EOF > "$filename"
 title: "$title"
 author: "Ally"
 summary: "$description"
-publishDate: $(date --iso-8601=seconds)
+publishDate: $(date +%Y-%m-%dT%H:%M:%S%z)
 tags: [$tags]
 draft: true
 ---
 EOF
-
-#---
-#title: "A command I've used a few times to install a fresh Laravel project into the cwd's src/ folder, leaving the cwd for your own README, Docker, Terraform, etc. files"
-#author: "Ally"
-#summary: "A command I've used a few times to install a fresh Laravel project into the cwd's src/ folder, leaving the cwd for your own README, Docker, Terraform, etc. files"
-#publishDate: 2021-05-14T19:25:30+01:00
-#tags: ['Laravel', 'Docker', 'PHP']
-#draft:true
-#---
