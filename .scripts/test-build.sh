46495bddafa67ad6d1200812d7e1f90f5787c11d
alistaircol <me@ac93.uk>
2021-07-12 20:35:36 +0100

    update build script

--- .scripts/test-build.sh
+++ .scripts/test-build.sh
@@ -4,7 +4,7 @@ tmpdir=$(mktemp -d)
 # 2: Interrupt
 # 3: Quit
 # 15: Terminate
-trap "rm -f $temp_file" 0 2 3 15
+trap "rm -f $tmpdir" 0 2 3 15
 
 git clone git@github.com:alistaircol/ac-netlify.git $tmpdir
 
@@ -18,3 +18,6 @@ EXPOSE 80
 EOF
 
 docker build --force-rm --tag=alistaircol/ac93 "$tmpdir"
+open "http://localhost:9999"
+
+docker run --rm -p 9999:80 alistaircol/ac93:latest
