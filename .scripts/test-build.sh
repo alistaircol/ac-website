3f38a847314eea4c5f04e399e02d081b28613cc8
alistaircol <aggenkeech@gmail.com>
2021-07-12 20:12:02 +0100

    add test build script

---
+++ .scripts/test-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+tmpdir=$(mktemp -d)
+# 0: exit shell
+# 2: Interrupt
+# 3: Quit
+# 15: Terminate
+trap "rm -f $temp_file" 0 2 3 15
+
+git clone git@github.com:alistaircol/ac-netlify.git $tmpdir
+
+docker run --rm --interactive --tty --user=$(id -u) --volume="$tmpdir:/src" klakegg/hugo:0.75.1-ext
+
+cat <<EOF > "$tmpdir/Dockerfile"
+FROM caddy:2-alpine
+WORKDIR /usr/share/caddy/
+COPY ./public /usr/share/caddy/
+EXPOSE 80
+EOF
+
+docker build --force-rm --tag=alistaircol/ac93 "$tmpdir"
